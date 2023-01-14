dcad67e722ca14437b8778e93483d6e7fc8942c9
Menemi <danila.titov1210@yandex.ru>
2023-01-14 18:42:00 +0300

    feat

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import sqlite3
 
 from aiogram import Bot, Dispatcher, executor, types
-from config import token, logs, path_to_db
+from config import token, logs, path_to_db, commands, buttons
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,12 +30,24 @@ def checker(message: types.Message):
 async def start(message: types.Message):
     log(message)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.InlineKeyboardButton("/dick")
-    markup.add(button)
-    button = types.InlineKeyboardButton("/top_dick")
-    markup.add(button)
+    for command in buttons:
+        button = types.InlineKeyboardButton(command)
+        markup.add(button)
+    await bot.send_message(message.from_user.id, text=f"Доступные команды:", reply_markup=markup)
+    return
 
-    await bot.send_message(message.from_user.id, text="Доступные команды:", reply_markup=markup)
+
+@dp.message_handler(commands=['help'])
+async def helper(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for command in buttons:
+        button = types.InlineKeyboardButton(command)
+        markup.add(button)
+    answer = ""
+    for command in commands:
+        answer += f"{command}\n"
+    await bot.send_message(
+        message.from_user.id, text=f"Доступные команды:\n{answer}", reply_markup=markup, parse_mode="HTML")
     return
 
 
@@ -124,6 +136,133 @@ async def top_dick(message: types.Message):
         count += 1
     await message.reply(answer, parse_mode="HTML")
     return
+
+
+@dp.message_handler(commands=['bet'])
+async def bet(message: types.Message):
+    log(message)
+    checker(message)
+    connection = sqlite3.connect(path_to_db)
+    cursor = connection.cursor()
+    args = message.get_args()
+    if not args:
+        await message.reply("Нужно ввести ставку: /bet <code>N</code>", parse_mode="HTML")
+        return
+
+    user_bet = str(args)
+    if user_bet.find(' ') != -1:
+        await message.reply("Ты че вообще еблан? Введи 1 аргумент, сука", parse_mode="HTML")
+        return
+    if user_bet.find(';') != -1 \
+            or user_bet.find('.') != -1 \
+            or user_bet.find(',') != -1 \
+            or user_bet.find(':') != -1 \
+            or not user_bet.isdigit():
+        await message.reply("Ставка должна быть в виде целого неотрицательного числа", parse_mode="HTML")
+        return
+
+    current_length = cursor.execute(
+        f"SELECT length FROM dicks WHERE user_id = {message.from_user.id}").fetchall()[0][0].split("-")
+    length = int(current_length[0])
+    username = current_length[1]
+
+    user_bet = int(user_bet)
+    if user_bet > length:
+        await message.reply(
+            f"Ставка не может быть больше твоего нынешнего размера писюна (<code>{length}</code>)", parse_mode="HTML")
+        return
+
+    luck = random.randint(0, 1)
+    final_length = length
+    text = ""
+    if luck == 1:
+        final_length = length + user_bet
+    else:
+        final_length = length - user_bet
+        text = "не"
+
+    cursor.execute(f'UPDATE dicks SET length = "{final_length}-{username}" WHERE user_id = {message.from_user.id}')
+    connection.commit()
+
+    dicks = cursor.execute(f"SELECT length FROM dicks").fetchall()
+    dicks_length = []
+
+    for dick in dicks:
+        dicks_length.append(int(dick[0].split('-')[0]))
+
+    expected_username = message.from_user.username
+    rating = 0
+    for i in range(len(dicks_length) - 1):
+        for j in range(len(dicks_length) - i - 1):
+            if dicks_length[j] < dicks_length[j + 1]:
+                dicks_length[j], dicks_length[j + 1] = dicks_length[j + 1], dicks_length[j]
+                dicks[j], dicks[j + 1] = dicks[j + 1], dicks[j]
+
+                if expected_username == dicks[j][0].split('-')[1]:
+                    rating = j
+                elif expected_username == dicks[j + 1][0].split('-')[1]:
+                    rating = j + 1
+
+    await message.reply(
+        f"@{message.from_user.username}, твоя ставка {text} сыграла\n"
+        f"Теперь твой писюн равен <b>{final_length}</b> см.\n"
+        f"Ты занимаешь <b>{rating}</b> место в топе\n", parse_mode="HTML")
+    return
+
+
+@dp.message_handler(commands=['all_in'])
+async def all_in(message: types.Message):
+    log(message)
+    checker(message)
+    connection = sqlite3.connect(path_to_db)
+    cursor = connection.cursor()
+
+    current_length = cursor.execute(
+        f"SELECT length FROM dicks WHERE user_id = {message.from_user.id}").fetchall()[0][0].split("-")
+    length = current_length[0]
+    username = current_length[1]
+
+    luck = random.randint(0, 1)
+    final_length = int(length)
+    if luck == 1:
+        final_length *= 2
+    else:
+        final_length = 0
+
+    cursor.execute(f'UPDATE dicks SET length = "{final_length}-{username}" WHERE user_id = {message.from_user.id}')
+    connection.commit()
+
+    dicks = cursor.execute(f"SELECT length FROM dicks").fetchall()
+    dicks_length = []
+
+    for dick in dicks:
+        dicks_length.append(int(dick[0].split('-')[0]))
+
+    expected_username = message.from_user.username
+    rating = 0
+    for i in range(len(dicks_length) - 1):
+        for j in range(len(dicks_length) - i - 1):
+            if dicks_length[j] < dicks_length[j + 1]:
+                dicks_length[j], dicks_length[j + 1] = dicks_length[j + 1], dicks_length[j]
+                dicks[j], dicks[j + 1] = dicks[j + 1], dicks[j]
+
+                if expected_username == dicks[j][0].split('-')[1]:
+                    rating = j
+                elif expected_username == dicks[j + 1][0].split('-')[1]:
+                    rating = j + 1
+
+    if luck == 1:
+        await message.reply(
+            f"@{message.from_user.username}, ебать тебе повезло-повезло, ты удвоил свой писюн\n"
+            f"Теперь твой писюн равен <b>{final_length}</b> см.\n"
+            f"Ты занимаешь <b>{rating}</b> место в топе\n", parse_mode="HTML")
+        return
+    else:
+        await message.reply(
+            f"@{message.from_user.username}, ебать ты лох, ты проебал весь свой писюн, АХАХХАХАХАХАХАХХАХАХАХАХ\n"
+            f"Теперь твой писюн равен <b>{final_length}</b> см.\n"
+            f"Ты занимаешь <b>{rating}</b> место в топе\n", parse_mode="HTML")
+        return
 
 
 @dp.message_handler(commands=['changesize'])
